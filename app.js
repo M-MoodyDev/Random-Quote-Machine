f9272c7a2158ee634cf4de5c63f04652ad124c80
Muhammad Huzaifa <75908442+M-MoodyDev@users.noreply.github.com>
2021-03-06 19:03:46 +0500

    Create app.js

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 /* JavaScript App */
+
+
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://andruxnet-random-famous-quotes.p.rapidapi.com/?cat=movies&count=10",
+	"method": "POST",
+	"headers": {
+		"x-rapidapi-key": "4950b89030msh4c28afa1db60111p195a38jsn4897232f033b",
+		"x-rapidapi-host": "andruxnet-random-famous-quotes.p.rapidapi.com"
+	}
+};
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
