331f05f2a75c3acc07272d469184459ef4f682ba
Muhammad Huzaifa <75908442+M-MoodyDev@users.noreply.github.com>
2021-03-06 22:41:32 +0500

    update app.js

--- app.js
+++ app.js
@@ -16,17 +16,63 @@ $.ajax(settings).done(function (response) {
 	console.log(response);
 });
 
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://andruxnet-random-famous-quotes.p.rapidapi.com/?cat=movies&count=10",
-	"method": "POST",
-	"headers": {
-		"x-rapidapi-key": "4950b89030msh4c28afa1db60111p195a38jsn4897232f033b",
-		"x-rapidapi-host": "andruxnet-random-famous-quotes.p.rapidapi.com"
-	}
-};
+function getRandomQuote() {
+  return quotesData.quotes[
+    Math.floor(Math.random() * quotesData.quotes.length)
+  ];
+}
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
+function getQuote() {
+  let randomQuote = getRandomQuote();
+
+  currentQuote = randomQuote.quote;
+  currentAuthor = randomQuote.author;
+
+  $('#tweet-quote').attr(
+    'href',
+    'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' +
+      encodeURIComponent('"' + currentQuote + '" ' + currentAuthor)
+  );
+
+  $('#tumblr-quote').attr(
+    'href',
+    'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=' +
+      encodeURIComponent(currentAuthor) +
+      '&content=' +
+      encodeURIComponent(currentQuote) +
+      '&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button'
+  );
+
+  $('.quote-text').animate({ opacity: 0 }, 500, function () {
+    $(this).animate({ opacity: 1 }, 500);
+    $('#text').text(randomQuote.quote);
+  });
+
+  $('.quote-author').animate({ opacity: 0 }, 500, function () {
+    $(this).animate({ opacity: 1 }, 500);
+    $('#author').html(randomQuote.author);
+  });
+
+  var color = Math.floor(Math.random() * colors.length);
+  $('html body').animate(
+    {
+      backgroundColor: colors[color],
+      color: colors[color]
+    },
+    1000
+  );
+  $('.button').animate(
+    {
+      backgroundColor: colors[color]
+    },
+    1000
+  );
+}
+
+$(document).ready(function () {
+  getQuotes().then(() => {
+    getQuote();
+  });
+
+  $('#new-quote').on('click', getQuote);
 });
